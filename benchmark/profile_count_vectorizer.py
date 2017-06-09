a6c11dc05f4c07abca21f53bbd7ad69a7bd7ee78
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-09 06:07:15 +0000

    Added profile_count_vectorizer.py

---
+++ benchmark/profile_count_vectorizer.py
@@ -0,0 +1,26 @@
+
+import timeit
+import pandas as pd
+from sklearn.feature_extraction.text import (CountVectorizer, TfidfVectorizer,
+                                             HashingVectorizer)
+
+from sklearn.datasets import fetch_20newsgroups
+from tqdm import tqdm
+import itertools
+import numpy as np
+
+import cProfile, pstats, io
+
+text = fetch_20newsgroups(subset='all').data
+
+cp = cProfile.Profile()
+cp.enable()
+
+vect = TfidfVectorizer(ngram_range=(1, 1))
+vect.fit_transform(text)
+
+s = io.StringIO()
+sortby = 'tottime'
+ps = pstats.Stats(cp, stream=s).sort_stats(sortby)
+ps.print_stats(100)
+print(s.getvalue())
