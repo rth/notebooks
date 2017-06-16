b01ae2503b3d8f6129c0b09a49ca9cc530171d09
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-16 13:51:17 +0100

    Add quick setup for EC2

---
+++ setup/setup_ec2.sh
@@ -0,0 +1,13 @@
+# Setup a miniconda environement on a new EC2 box
+
+sudo apt-get install -y gcc g++ screen vim wget
+
+#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+#chmod +x miniconda.sh
+#./miniconda.sh -b
+#echo 'export PATH=/home/travis/miniconda2/bin:$PATH' > ~/.bashrc
+#source ~/.bashrc
+
+wget https://raw.githubusercontent.com/rth/notebooks/master/notebooks/setup/.screenrc -O ~/.screenrc
+
+
