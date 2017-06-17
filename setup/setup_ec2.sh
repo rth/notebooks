b524ab2c3b4834ed31ec717cb56e123fbacc0d15
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-17 16:41:45 +0100

    Update setup_ec2.sh

--- setup/setup_ec2.sh
+++ setup/setup_ec2.sh
@@ -1,6 +1,6 @@
 # Setup a miniconda environement on a new EC2 box
 
-sudo apt-get install -y gcc g++ screen vim wget
+sudo apt-get install -y gcc g++ screen vim wget htop
 
 #wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 #chmod +x miniconda.sh
@@ -9,5 +9,8 @@ sudo apt-get install -y gcc g++ screen vim wget
 #source ~/.bashrc
 
 wget https://raw.githubusercontent.com/rth/notebooks/master/setup/.screenrc -O ~/.screenrc
+
+git config --global user.name "Roman Yurchak"
+git config --global user.email "rth.yurchak@gmail.com"
 
 
