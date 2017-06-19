7d6a7645cf973fef8c1b496056ceef2798019287
Roman Yurchak <rth.yurchak@gmail.com>
2017-06-19 12:51:16 +0300

    Added test for issue 2969

--- sklearn/issue_2969/test_64bit_csr_indices.py
+++ sklearn/issue_2969/test_64bit_csr_indices.py
@@ -5,8 +5,8 @@ import itertools
 
 np.random.seed(42)
 
-n_samples = 100
-n_features = 10000
+n_samples = 10
+n_features = 1000
 
 
 def init_arrays():
@@ -14,7 +14,7 @@ def init_arrays():
 
     X.indices = X.indices.astype('int64')
     X.indptr = X.indptr.astype('int64')
-    y = np.random.randint(0, 10, size=(n_samples,))
+    y = np.random.randint(0, 3, size=(n_samples,))
     return X, y
 
 
@@ -44,9 +44,20 @@ def test_decomposition_truncatedsvd(algorithm):
     svd.transform(X)
 
 
+@pytest.mark.parametrize('solver', ['cd', 'mu'])
+def test_decomposition_nmf(solver):
+    from sklearn.decomposition import NMF
+
+    X, y = init_arrays()
+
+    svd = NMF(solver=solver)
+    svd.fit(X)
+    svd.transform(X)
+
+
 @pytest.mark.parametrize('solver, penalty',
-                         [#('liblinear', 'l1'),
-                          #('liblinear', 'l2'),
+                         [('liblinear', 'l1'),
+                          ('liblinear', 'l2'),
                           ('newton-cg', 'l2'),
                           ('lbfgs', 'l2'),
                           ('sag', 'l2')])
@@ -76,11 +87,33 @@ def test_linear_model_sgdclassifier(loss, penalty):
     cl.predict(X)
 
 
-@pytest.mark.parametrize('estimator_name', ['LinearRegression', 'ElasticNet'])
+@pytest.mark.parametrize('loss, penalty',
+                         itertools.product(['squared_loss', 'huber',
+                                            'epsilon_insensitive',
+                                            'squared_epsilon_insensitive'],
+                                           ['l1', 'l2', 'elasticnet']))
+def test_linear_model_sgdregressor(loss, penalty):
+    from sklearn.linear_model import SGDClassifier
+
+    X, y = init_arrays()
+
+    cl = SGDClassifier(penalty=penalty, loss=loss)
+    cl.fit(X, y)
+
+    cl.predict(X)
+
+
+@pytest.mark.parametrize('estimator_name', ['linear_model.LinearRegression',
+                                            'linear_model.ElasticNet',
+                                            'svm.LinearSVC',
+                                            'svm.SVC',
+                                            'tree.DecisionTreeClassifier'])
 def test_linear_model_estimator(estimator_name):
+    import sklearn.tree
+    import sklearn.svm
     import sklearn.linear_model
 
-    Estimator = getattr(sklearn.linear_model, estimator_name)
+    Estimator = eval('sklearn.' + estimator_name)
 
     X, y = init_arrays()
 
