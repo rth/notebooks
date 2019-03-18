0edcc750ed08ff3498f504e2bb6c7bc6b3c1201e
Roman Yurchak <rth.yurchak@pm.me>
2019-03-18 09:59:12 +0100

    Update setup_ec2.sh

--- setup/setup_ec2.sh
+++ setup/setup_ec2.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y gcc g++ screen vim wget htop git make
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
-echo 'export PATH=~/miniconda3/bin:$PATH' > ~/.bashrc
+echo 'export PATH=~/miniconda3/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
 wget https://raw.githubusercontent.com/rth/notebooks/master/setup/.screenrc -O ~/.screenrc
